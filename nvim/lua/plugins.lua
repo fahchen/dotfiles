7a04208ff80961f629c0928c72ea7f8eb62c224b
Phil Chen <06fahchen@gmail.com>
2022-05-14 23:07:07 +0800

    tweak lualine

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -127,13 +127,13 @@ require('packer').startup(function()
 	},
 	inactive_sections = {
 	  lualine_a = {},
-	  lualine_b = {},
+	  lualine_b = {'diff'},
 	  lualine_c = {{
 	    'filename',
 	    file_status = true, -- displays file status (readonly status, modified status)
 	    path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
 	  }},
-	  lualine_x = {'location'},
+	  lualine_x = {'filetype'},
 	  lualine_y = {},
 	  lualine_z = {}
 	},
