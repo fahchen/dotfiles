d523475010d93298a00cf2ab979bbf62a473f87a
Phil Chen <06fahchen@gmail.com>
2023-06-04 22:01:42 +0800

    feat(vim): make the lua ls aware of Neovim runtime files

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -459,13 +459,38 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = { "html", "jsonls", "cssls", "tailwindcss", "sqlls", "bashls", "gopls", "lua_ls" }
+local servers = { "html", "jsonls", "cssls", "tailwindcss", "sqlls", "bashls", "gopls" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = common_on_attach,
     capabilities = capabilities,
   }
 end
+
+nvim_lsp.lua_ls.setup {
+  on_attach = common_on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 nvim_lsp.rust_analyzer.setup {
   on_attach = common_on_attach,
