0282ef394001d6047dd688bd98ba6b839ee4fe75
Phil Chen <06fahchen@gmail.com>
2024-03-30 10:47:41 +0800

    feat: add catppuccin colorscheme

--- lazyvim/lua/plugins/colors.lua
+++ lazyvim/lua/plugins/colors.lua
@@ -1,8 +1,15 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd([[colorscheme tokyonight]])
-  end,
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  }
 }
