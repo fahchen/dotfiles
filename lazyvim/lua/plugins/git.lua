bff380f40233c27e58ca45ca25774fc9c10fe378
Phil Chen <06fahchen@gmail.com>
2024-05-22 15:05:24 +0800

    feat(nvim): set map for vim-gh-line

--- lazyvim/lua/plugins/git.lua
+++ lazyvim/lua/plugins/git.lua
@@ -1,4 +1,13 @@
 return {
   { "wsdjeg/vim-fetch" },
-  { "ruanyl/vim-gh-line" },
+  {
+    "ruanyl/vim-gh-line",
+    init = function()
+      vim.g.gh_repo_map = '<leader>go'
+      vim.g.gh_line_map = '<leader>gl'
+      vim.g.gh_line_blame_map = '<leader>gb'
+      vim.g.gh_use_canonical = 1
+      vim.g.gh_open_command = 'function fn; echo -n $argv | pbcopy; end; fn ' -- fish shell
+    end
+  },
 }
