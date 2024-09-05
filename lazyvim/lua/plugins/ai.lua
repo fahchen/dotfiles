912ea47459f76462cfc5544e3c38d65bf4e7f431
Phil Chen <06fahchen@gmail.com>
2024-09-05 10:28:00 +0800

    feat(nvim): update ai plugin

--- lazyvim/lua/plugins/ai.lua
+++ lazyvim/lua/plugins/ai.lua
@@ -2,15 +2,33 @@ return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
+    lazy = false,
+    version = false,
     opts = {
       provider = "copilot",
     },
+    build = "make",
     dependencies = {
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      --- The below is optional, make sure to setup it properly if you have lazy=true
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+          },
+        },
+      },
       {
         'MeanderingProgrammer/render-markdown.nvim',
         opts = {
