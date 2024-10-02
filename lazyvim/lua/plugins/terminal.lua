e9d1c7a7eacb960833e427fc37cfae2fda357a55
Phil Chen <06fahchen@gmail.com>
2024-10-02 13:13:03 +0800

    feat(nvim): change FTerm mapping to c-*

--- lazyvim/lua/plugins/terminal.lua
+++ lazyvim/lua/plugins/terminal.lua
@@ -2,8 +2,8 @@ return {
   {
     "numToStr/FTerm.nvim",
     keys = {
-      { "<m-i>", '<CMD>lua require("FTerm").toggle()<CR>',            desc = "open terminal",  mode = "n" },
-      { "<m-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', desc = "close terminal", mode = "t" },
+      { "<c-t>", '<CMD>lua require("FTerm").toggle()<CR>',            desc = "open terminal",  mode = "n" },
+      { "<c-t>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', desc = "close terminal", mode = "t" },
     },
   },
 }
