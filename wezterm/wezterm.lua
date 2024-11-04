321144d9c4cfce2eaad96c554fa9f519beeb44eb
Phil Chen <06fahchen@gmail.com>
2024-11-04 16:46:31 +0800

    feat(wezterm): update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,100 +1,123 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 local config = {}
 
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Tokyo Night Moon"
+	else
+		-- return "Tokyo Night Day"
+		return "Tokyo Night Day"
+	end
+end
+
 -- Show which key table is active in the status area
-wezterm.on('update-right-status', function(window)
-  local name = window:active_key_table()
-  if name then
-    name = 'TABLE: ' .. name
-  end
-  window:set_right_status(name or '')
+wezterm.on("update-right-status", function(window)
+	local name = window:active_key_table()
+	if name then
+		name = "TABLE: " .. name
+	end
+	window:set_right_status(name or "")
 end)
 
+-- appearance
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono",
+	"Terminus",
+	"Noto Color Emoji",
+	"Microsoft YaHei",
+})
+
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = true
+
 -- timeout_milliseconds defaults to 1000 and can be omitted
-config.leader = { key = 'Space', mods = 'CMD|CTRL|OPT|SHIFT', timeout_milliseconds = 1000 }
+config.leader = { key = "Space", mods = "CMD|CTRL|OPT|SHIFT", timeout_milliseconds = 1000 }
 config.keys = {
-  {
-    key = '_',
-    mods = 'LEADER|SHIFT',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = '-',
-    mods = 'LEADER',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
+	{
+		key = "_",
+		mods = "LEADER|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
 
-  -- LEADER, followed by 'z' will toggle currentPane's zoom
-  {
-    key = 'z',
-    mods = 'LEADER',
-    action = wezterm.action.TogglePaneZoomState,
-  },
+	-- LEADER, followed by 'z' will toggle currentPane's zoom
+	{
+		key = "z",
+		mods = "LEADER",
+		action = wezterm.action.TogglePaneZoomState,
+	},
 
-  -- LEADER, followed by 'r' will put us in resize-pane
-  -- mode until we cancel that mode.
-  {
-    key = 'r',
-    mods = 'LEADER',
-    action = act.ActivateKeyTable {
-      name = 'resize_pane',
-      one_shot = false,
-    },
-  },
+	-- LEADER, followed by 'r' will put us in resize-pane
+	-- mode until we cancel that mode.
+	{
+		key = "r",
+		mods = "LEADER",
+		action = act.ActivateKeyTable({
+			name = "resize_pane",
+			one_shot = false,
+		}),
+	},
 
-  -- LEADER, followed by 'a' will put us in activate-pane
-  -- mode until we press some other key or until 1 second (1000ms)
-  -- of time elapses
-  {
-    key = 'a',
-    mods = 'LEADER',
-    action = act.ActivateKeyTable {
-      name = 'activate_pane',
-      timeout_milliseconds = 1000,
-    },
-  },
+	-- LEADER, followed by 'a' will put us in activate-pane
+	-- mode until we press some other key or until 1 second (1000ms)
+	-- of time elapses
+	{
+		key = "a",
+		mods = "LEADER",
+		action = act.ActivateKeyTable({
+			name = "activate_pane",
+			timeout_milliseconds = 1000,
+		}),
+	},
 
-  {
-    key = 'p',
-    mods = 'CMD',
-    action = wezterm.action.ActivateCommandPalette,
-  },
+	{
+		key = "p",
+		mods = "CMD",
+		action = wezterm.action.ActivateCommandPalette,
+	},
 }
 
 config.key_tables = {
-  -- Defines the keys that are active in our resize-pane mode.
-  -- Since we're likely to want to make multiple adjustments,
-  -- we made the activation one_shot=false. We therefore need
-  -- to define a key assignment for getting out of this mode.
-  -- 'resize_pane' here corresponds to the name="resize_pane" in
-  -- the key assignments above.
-  resize_pane = {
-    { key = 'LeftArrow',  action = act.AdjustPaneSize { 'Left', 1 } },
-    { key = 'h',          action = act.AdjustPaneSize { 'Left', 1 } },
-    { key = 'RightArrow', action = act.AdjustPaneSize { 'Right', 1 } },
-    { key = 'l',          action = act.AdjustPaneSize { 'Right', 1 } },
-    { key = 'UpArrow',    action = act.AdjustPaneSize { 'Up', 1 } },
-    { key = 'k',          action = act.AdjustPaneSize { 'Up', 1 } },
-    { key = 'DownArrow',  action = act.AdjustPaneSize { 'Down', 1 } },
-    { key = 'j',          action = act.AdjustPaneSize { 'Down', 1 } },
+	-- Defines the keys that are active in our resize-pane mode.
+	-- Since we're likely to want to make multiple adjustments,
+	-- we made the activation one_shot=false. We therefore need
+	-- to define a key assignment for getting out of this mode.
+	-- 'resize_pane' here corresponds to the name="resize_pane" in
+	-- the key assignments above.
+	resize_pane = {
+		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "UpArrow", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "DownArrow", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
 
-    -- Cancel the mode by pressing escape
-    { key = 'Escape',     action = 'PopKeyTable' },
-  },
-  -- Defines the keys that are active in our activate-pane mode.
-  -- 'activate_pane' here corresponds to the name="activate_pane" in
-  -- the key assignments above.
-  activate_pane = {
-    { key = 'LeftArrow',  action = act.ActivatePaneDirection 'Left' },
-    { key = 'h',          action = act.ActivatePaneDirection 'Left' },
-    { key = 'RightArrow', action = act.ActivatePaneDirection 'Right' },
-    { key = 'l',          action = act.ActivatePaneDirection 'Right' },
-    { key = 'UpArrow',    action = act.ActivatePaneDirection 'Up' },
-    { key = 'k',          action = act.ActivatePaneDirection 'Up' },
-    { key = 'DownArrow',  action = act.ActivatePaneDirection 'Down' },
-    { key = 'j',          action = act.ActivatePaneDirection 'Down' },
-  },
+		-- Cancel the mode by pressing escape
+		{ key = "Escape", action = "PopKeyTable" },
+	},
+	-- Defines the keys that are active in our activate-pane mode.
+	-- 'activate_pane' here corresponds to the name="activate_pane" in
+	-- the key assignments above.
+	activate_pane = {
+		{ key = "LeftArrow", action = act.ActivatePaneDirection("Left") },
+		{ key = "h", action = act.ActivatePaneDirection("Left") },
+		{ key = "RightArrow", action = act.ActivatePaneDirection("Right") },
+		{ key = "l", action = act.ActivatePaneDirection("Right") },
+		{ key = "UpArrow", action = act.ActivatePaneDirection("Up") },
+		{ key = "k", action = act.ActivatePaneDirection("Up") },
+		{ key = "DownArrow", action = act.ActivatePaneDirection("Down") },
+		{ key = "j", action = act.ActivatePaneDirection("Down") },
+	},
 }
 
 return config
